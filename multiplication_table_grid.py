f80f768f2b744ef14215b20077054e043d47bcb7
humeraminhaj <itishumera@gmail,com>
2026-05-26 12:06:06 +0530

    testing merge conflicts

--- multiplication_table_grid.py
+++ multiplication_table_grid.py
@@ -1,15 +1,15 @@
 while True:
-    print('please enter a number to get the multiplication table grid:')
+    print('Please enter a number to get the multiplication table grid:')
     num=int(input())
     for i in range(1,num+1):
         tablerow=[]
         for j in range(1,num+1):
             a=i*j
-            tablerow.append(f"{a:3}")
+            tablerow.append(f"{a:4}")
         result=' '.join(tablerow)
         print(result)
-    print('do you want to continue? (y/n)')
+    print('Do you want to continue? (y/n)')
     choice=input()  
     if choice.lower()=='n':
-        print('thank you for using the multiplication table grid generator!')
+        print('Thank You for using the multiplication table grid generator!')
         break
